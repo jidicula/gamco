d24f941fbce56004b5cfeee1c1cc5af272bde981
jidicula <johanan@forcepush.tech>
2021-04-03 17:31:19 -0400

    test(getData): Add regex test for response
    Resolves: #1

--- gamco.go
+++ gamco.go
@@ -1,3 +1,10 @@
 // Package gamco provides an unofficial API wrapper for GAMCO's Closed-End
 // Funds API
 package gamco
+
+// getData hits the nav_closed_ends endpoint.
+func getData() string {
+	var response string
+
+	return response
+}
--- gamco_test.go
+++ gamco_test.go
@@ -1,1 +1,27 @@
 package gamco
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestGetData(t *testing.T) {
+	tests := map[string]struct {
+		want *regexp.Regexp
+	}{
+		"api test": {
+			want: regexp.MustCompile(`\[(\{(\".*\":.*,?)+\},?)+\]`),
+		},
+	}
+
+	for name, tt := range tests {
+
+		t.Run(name, func(t *testing.T) {
+
+			got := getData()
+			if !tt.want.MatchString(got) {
+				t.Errorf("%s: got %s, want `%s` regex match", name, got, tt.want.String())
+			}
+		})
+	}
+}
